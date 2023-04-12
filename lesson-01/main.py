cd369ab5212161dd325edd4a54cad0efa340fad9
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2023-04-12 20:06:41 +0000

    Lesson 01

---
+++ lesson-01/main.py
@@ -0,0 +1,58 @@
+from duckduckgo_search import ddg_images
+from fastcore.all import *
+from fastdownload import download_url
+from fastai.vision.all import *
+from time import sleep
+
+def search_images(term, max_images=30):
+    print(f"Searching for '{term}'")
+    return L(ddg_images(term, max_results=max_images)).itemgot('image')
+
+urls = search_images('bird photos', max_images=1)
+print(urls[0])
+
+dest = 'bird.jpg'
+download_url(urls[0], dest, show_progress=False)
+
+im = Image.open(dest)
+im.to_thumb(256,256)
+
+download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
+Image.open('forest.jpg').to_thumb(256,256)
+
+searches = 'forest','bird'
+path = Path('bird_or_not')
+
+for o in searches:
+    dest = (path/o)
+    dest.mkdir(exist_ok=True, parents=True)
+    download_images(dest, urls=search_images(f'{o} photo'))
+    sleep(10)  # Pause between searches to avoid over-loading server
+    download_images(dest, urls=search_images(f'{o} sun photo'))
+    sleep(10)
+    download_images(dest, urls=search_images(f'{o} shade photo'))
+    sleep(10)
+    resize_images(path/o, max_size=400, dest=path/o)
+
+failed = verify_images(get_image_files(path))
+failed.map(Path.unlink)
+print("Number of failed images:")
+print(len(failed))
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path, bs=32)
+
+dls.show_batch(max_n=6)
+
+print("Building a model")
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(3)
+
+is_bird,_,probs = learn.predict(PILImage.create('bird.jpg'))
+print(f"This is a: {is_bird}.")
+print(f"Probability it's a bird: {probs[0]:.4f}")
